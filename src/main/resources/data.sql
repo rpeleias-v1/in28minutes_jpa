384660d5c3bacce6211cc66f0e1f20d9f5b8490f
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-02-25 22:39:51 -0300

    Developed 23 step

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ insert into course(id, name, created_date, last_updated_date) values (10001, 'JP
 insert into course(id, name, created_date, last_updated_date) values (10002, 'Spring in 50 steps', sysdate(), sysdate());
 insert into course(id, name, created_date, last_updated_date) values (10003, 'Spring Boot in 50 steps', sysdate(), sysdate());
 
-insert into student(id,name) values(20001, 'Ranga');
-insert into student(id,name) values(20002, 'Adam');
-insert into student(id,name) values(20003, 'Jane');
-
 insert into passport(id,number) values(40001, 'E123456');
 insert into passport(id,number) values(40002, 'N789601');
 insert into passport(id,number) values(40003, 'J234454');
+
+insert into student(id,name, passport_id) values(20001, 'Ranga', 40001);
+insert into student(id,name, passport_id) values(20002, 'Adam', 40002);
+insert into student(id,name, passport_id) values(20003, 'Jane', 40003);
 
 insert into review(id,rating, description) values(50001, '5', 'Great Course');
 insert into review(id,rating, description) values(50002, '4', 'Wonderful Course');
