a081f78d6877eacf5686c594f072839391ebff77
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-02-25 23:02:19 -0300

    Developed 27 step

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.in28minutes.jpa.hibernate.demo.repository;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
+import com.in28minutes.jpa.hibernate.demo.entity.Passport;
 import com.in28minutes.jpa.hibernate.demo.entity.Student;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
@@ -27,6 +29,21 @@ public class StudentRepositoryTest {
 
         Assert.assertNotNull(student);
         Assert.assertNotNull(student.getPassport());
+    }
+
+    @Test
+    @Transactional
+    public void someTest() {
+        someDummyOperation();
+    }
+
+    private void someDummyOperation() {
+        Student student = entityManager.find(Student.class, 20001L);
+
+        Passport passport = student.getPassport();
+
+        passport.setNumber("E123457");
+        student.setName("Ranga - updated");
     }
 
 }
